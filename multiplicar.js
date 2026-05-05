cf48c63a17415552684d00e7b46a39f67245a868
JHOON231 <jhondavis.quinterot@gmail.com>
2026-05-05 14:24:37 -0500

    se mejoro el estilo

--- multiplicar.js
+++ multiplicar.js
@@ -1,10 +1,26 @@
 function generarTablas(){
     let contenedor = document.getElementById("contenedor");
-    let contenido = "";
-    let numero = document.getElementById("txtNumero").value;
-    for(let i =0; i <= 10; i++){
-         contenido += "<div class='fila'>5 x " + i + " = " + (5 * i) + "</div>";
+    let numero = parseInt(document.getElementById("txtNumero").value);
+
+    if (isNaN(numero)) {
+        contenedor.innerHTML = "<p style='color: yellow;'> Escribe un número primero </p>";
+        return;
     }
 
-     contenedor.innerHTML = contenido;
+    let contenido = `<h2> Tabla del ${numero} </h2>`;
+
+    for(let i =0; i <= 10; i++){
+        contenido += "<div class='fila'> " + numero + " x " + i + " = " + (numero * i) + "</div>";
+    }
+
+    contenedor.innerHTML = contenido;
+}
+
+function volver() {
+    let contenedor = document.getElementById("contenedor");
+    let input = document.getElementById("txtNumero");
+
+    contenedor.innerHTML = ""; 
+    input.value = "";          
+    input.focus();             
 }
