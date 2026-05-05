a5eb8dc9b7a81e1d07302e6e4c2f0c6b4ffb0565
JHOON231 <jhondavis.quinterot@gmail.com>
2026-05-05 13:52:30 -0500

    se agrego interaccion con el usuario

--- multiplicar.js
+++ multiplicar.js
@@ -1,6 +1,7 @@
 function generarTablas(){
     let contenedor = document.getElementById("contenedor");
     let contenido = "";
+    let numero = document.getElementById("txtNumero").value;
     for(let i =0; i <= 10; i++){
          contenido += "<div class='fila'>5 x " + i + " = " + (5 * i) + "</div>";
     }
